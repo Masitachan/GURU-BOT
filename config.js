a18250efbe2a99a27e3429c77bd7e440e11dd051
Masitachan <147782942+Masitachan@users.noreply.github.com>
2023-12-06 20:39:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['917788861856', 'masita bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
